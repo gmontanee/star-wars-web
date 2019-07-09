2cf4ddb3c2ac722483632da14b0d7f8500a8bab2
Gerard Montane <gmontanee@gmail.com>
2019-07-09 15:22:02 +0200

    javascript done

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,2 +1,18 @@
 'use strict';
 
+function LandingPage (parentElement) {
+  this.parentElement = parentElement;
+  this.elements = null;
+}
+
+LandingPage.prototype.generate = function () {
+  this.elements = `
+      <header>
+      <h1>Super cool Star Wars website </h1>
+      <h2>Here you can find the information to become the best fan of Star Wars </h2>
+      </header>`;
+  this.render();
+}
+LandingPage.prototype.render = function() {
+  this.parentElement.innerHTML = this.elements;
+}
